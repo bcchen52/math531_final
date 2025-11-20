a45520ac7bae7e5e37694106289cbafe6727d69a
bchen17 <bchen17@binghamton.edu>
2025-11-20 18:15:26 -0500

    iso_code usage

--- final.r
+++ final.r
@@ -25,13 +25,20 @@ year_na_summary <- raw_data %>%
 
 #print(year_na_summary, n = Inf)
 
+#unique_countries = unique(raw_data$country)
+unique_countries = unique(raw_data$iso_code)
+
 # 1851 is where there are 252 countries total
-data0 = raw_data %>% filter(year >= 1851)
+data0 = raw_data %>% filter(year >= 1851) %>% filter(iso_code != "", !is.na(iso_code))
+# there is an empty string for iso_code as well as NA
+
+unique_countries = unique(data0$country)
+
 
 data1 = data0 %>% drop_na(co2_per_capita)
 
 #this is the cleaned data
-data1 = data1 %>% mutate(country = factor(country), gdp_per_capita = gdp/population, trade_co2_per_capita = trade_co2/population)
+data1 = data1 %>% mutate(iso_code = factor(iso_code), gdp_per_capita = gdp/population, trade_co2_per_capita = trade_co2/population)
 
 #country
 #population
@@ -49,9 +56,11 @@ data1 = data1 %>% mutate(country = factor(country), gdp_per_capita = gdp/populat
 #ghg_per_capita
 
 wanted_columns = c(
-    "country", 
+    "iso_code", 
     "co2_per_capita", 
     "gdp_per_capita",
+    "gdp",
+    "population",
     "cement_co2_per_capita",
     "coal_co2_per_capita",
     "oil_co2_per_capita",
@@ -72,19 +81,29 @@ kaya_identity = c(
     "co2_per_unit_energy"
 )
 
-data2 = data1 %>% select(all_of(wanted_columns)) %>% drop_na()
+data2 = data1 %>% select(all_of(wanted_columns))
 
-data3 = data1 %>% select(all_of(kaya_identity)) %>% drop_na()
+data2_numeric = data2 %>% select(where(is.numeric)) %>% drop_na()
 
-model0 = lm(co2_per_capita ~ ., data=data2)
+str(data2_numeric)
 
-model1 = lm(co2 ~ ., data=data3)
+head(data2_numeric)
 
-summary(data2)
-
-summary(model1)
-
-data2_numeric = data2 %>% select(where(is.numeric))
+tail(data2_numeric)
 
 cor_mat <- cor(data2_numeric)
 cor_mat
+
+#data3 = data1 %>% select(all_of(kaya_identity)) %>% drop_na()
+
+data_c = unique(data2$country)
+
+data_c
+
+model0 = lm(co2_per_capita ~ ., data=data2)
+
+#model1 = lm(co2 ~ ., data=data3)
+
+#summary(data2)
+
+#summary(model0)
